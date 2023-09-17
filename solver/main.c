fa661d5f3fc82be78a5091970a4157773b54b6c5
Elena <gasouvay@gmail.com>
2023-09-17 22:52:07 +0100

    added file imports for easier testing

--- solver/main.c
+++ solver/main.c
@@ -1,15 +1,38 @@
 #include "print-sudoku.h"
 #include "sudoku.h"
 #include <stddef.h>
-
+#include <stdio.h>
 
 int main(void)
 {
 	unsigned int tab[9][9];
-	for(size_t x = 0; x < 9; x++)
-		for(size_t y = 0; y < 9; y++)
-			tab[x][y] = 2;
+	//for(size_t x = 0; x < 9; x++)
+	//	for(size_t y = 0; y < 9; y++)
+	//		tab[x][y] = 2;
+	FILE* ptr;
+	char c;
+	
+	//extract board from ./board
 
+	ptr = fopen("board","r");
+	size_t x = 0;
+	size_t y = 0;
+	while (!feof(ptr))
+	{
+		c = fgetc(ptr);
+		if (c == '\n')
+		{
+			x = 0;
+			y++;
+		}
+		else
+			tab[y][x++] = c - '0';
+	}
+
+
+	//display board
 	print_sudoku(tab);
+
+
 	return 0;
 }
