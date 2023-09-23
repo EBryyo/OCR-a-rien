9ef8d798f8286cd708c5e32383619c62cdb23c7e
elena <gaspard.souvay@epita.fr>
2023-09-23 18:12:36 +0100

    replaced position array with position pointers

--- solver/final/sudoku.c
+++ solver/final/sudoku.c
@@ -2,163 +2,138 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-unsigned int check_line (size_t pos[2], unsigned int tab[][9])
+unsigned int check_line (size_t* x, size_t* y, unsigned int tab[][9])
 { 
 	//checks if line is valid
 	
-	unsigned int x = pos[1];
-	unsigned int y = pos[0];
-	unsigned int cur = tab[y][x];
-	
+	unsigned int cur = tab[*y][*x];
 	for(size_t X = 0; X < 9; X++)
 	{
-		if (X != x && tab[y][X] == cur)
+		if (X != *x && tab[*y][X] == cur)
 			return 0;
 	}
 	return 1;
 }
 
-unsigned int check_column (size_t pos[2], unsigned int tab[][9])
+unsigned int check_column (size_t* x, size_t* y, unsigned int tab[][9])
 {
 	//checks if column is valid
 	
-	size_t x = pos[1];
-	size_t y = pos[0];
-	unsigned int cur = tab[y][x];
-	
+	unsigned int cur = tab[*y][*x];
 	for(size_t Y = 0; Y < 9; Y++)
 	{
-		if (Y != y && tab[Y][x] == cur)
+		if (Y != *y && tab[Y][*x] == cur)
 			return 0;
 	}
 	return 1;
 }
 
-unsigned int check_box (size_t pos[2], unsigned int tab[][9])
+unsigned int check_box (size_t* x, size_t* y, unsigned int tab[][9])
 {
 	//checks if box is valid
 	
-	size_t x = pos[1];
-	size_t y = pos[0];
-	unsigned int cur = tab[y][x];
-	
-	size_t xbias = (x / 3) * 3;
-	size_t ybias = (y / 3) * 3;
+	unsigned int cur = tab[*y][*x];
+	size_t xbias = (*x / 3) * 3;
+	size_t ybias = (*y / 3) * 3;
 	
 	for (size_t i = ybias; i < ybias + 3; i++)
 	{
 		for (size_t j = xbias; j < xbias + 3; j++)
 		{
-			if(!(j == x && i == y) && tab[i][j] == cur)
+			if(!(j == *x && i == *y) && tab[i][j] == cur)
 				return 0;
 		}
 	}
 	return 1;
 }
 
-void next (size_t pos[2], unsigned int ref[][9])
+void next (size_t* x, size_t* y, unsigned int ref[][9])
 {
 	//moves pos to the next mutable position
-	size_t x = pos[1];
-	size_t y = pos[0];
 	do {
-		x = (x + 1) % 9;
-		y += !(x);
-	} while (ref[y][x]);
-	pos[1] = x;
-	pos[0] = y;
+		*x = (*x + 1) % 9;
+		*y += !(*x);
+	} while (ref[*y][*x]);
 }
 
-void prev (size_t pos[2], unsigned int ref[][9])
+void prev (size_t* x, size_t* y, unsigned int ref[][9])
 {
 	//moves pos to the previous mutable position
-	size_t x = pos[1];
-	size_t y = pos[0];
 	do {
-		if (!x)
+		if (!*x)
 		{
-			y--;
-			x = 8;
+			*y = *y - 1;
+			*x = 8;
 		}
-		else 
-			x--;
-	} while (ref[y][x]);
-	pos[1] = x;
-	pos[0] = y;
+		else
+			*x = *x - 1;
+	} while (ref[*y][*x]);
 }
 
-unsigned int possible (size_t pos[2], unsigned int tab[][9])
+unsigned int possible (size_t* x, size_t* y, unsigned int tab[][9])
 { 
 	//returns 0 if the current state of the board is impossible, else not 0
 	//printf("exec: possible\n");
-	return (check_line(pos, tab) && check_column(pos, tab)) && check_box(pos, tab);
+	return (check_line(x, y, tab) && check_column(x, y, tab))
+		&& check_box(x, y, tab);
 }
 
-void mv_next (size_t pos[2], unsigned int tab[][9], unsigned int ref[][9])
+void mv_next (size_t* x, size_t* y, unsigned int tab[][9], unsigned int ref[][9])
 { 
 	//moves the program forward in the board
-	
-	size_t x = pos[1];
-	size_t y = pos[0];
-	
-	if (possible(pos,tab))
+
+	if (possible(x, y, tab))
 	{
-		next(pos,ref);
-		tab[pos[0]][pos[1]]++;
+		next(x, y, ref);
+		tab[*y][*x]++;
 	}
-	else tab[y][x]++;
+	else tab[*y][*x]++;
 }
 
-void mv_prev (size_t pos[2], unsigned int tab[][9], unsigned int ref[][9])
+void mv_prev (size_t* x, size_t* y, unsigned int tab[][9], unsigned int ref[][9])
 { 
 	//backtracks to next iterable state
-	
-	size_t x = pos[1];
-	size_t y = pos[0];
-	
+
 	do {
-		tab[y][x] = 0;
-		prev(pos,ref);
-		x = pos[1];
-		y = pos[0];
-	} while (tab[y][x] == 9);
-	tab[y][x]++;
+		tab[*y][*x] = 0;
+		prev(x, y, ref);
+	} while (tab[*y][*x] == 9);
+	tab[*y][*x]++;
 }
 
 
 void solve (unsigned int tab[][9])
 {
 	//solving process
-	unsigned int ref[9][9]; 
+	unsigned int ref[9][9];
 	//deep copy of tab,
 	//as to know which cells are immutable and which are not.
 	for(size_t x = 0; x < 9; x++)
 		for(size_t y = 0; y < 9; y++)
 			ref[x][y] = tab[x][y];
-	size_t p[] = {0,0}; //array representing the current position
+	size_t posx = 0;
+	size_t posy = 0;
+	//array representing the current position
 	while(1)
 	{
 			//iteration
-		size_t x = p[1];
-		size_t y = p[0];
 		
-		if (y > 8)
+		if (posy > 8)
 		{
-			p[1] = 8;
-			p[0] = 8;
-			if (possible(p,tab))
+			posy = 8;
+			posx = 8;
+			if (possible(&posx, &posy, tab))
 				break;
-			else 
+			else
 			{
 				if (ref[8][8])
-					prev(p,ref);
-				mv_prev(p,tab,ref);
+					prev(&posx, &posy,ref);
+				mv_prev(&posx, &posy,tab,ref);
 			}
 		}
-		if (!possible(p,tab) && tab[y][x] == 9)
-			mv_prev(p,tab,ref);
-		else 
-			mv_next(p,tab,ref);
+		if (!possible(&posx, &posy, tab) && tab[posy][posx] == 9)
+			mv_prev(&posx, &posy, tab,ref);
+		else
+			mv_next(&posx ,&posy, tab,ref);
 	}
-	
 }
--- solver/hex/sudoku.c
+++ solver/hex/sudoku.c
@@ -2,127 +2,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int check_line (size_t pos[2], int tab[][16])
-{ //done
+int check_line (size_t* x, size_t* y, int tab[][16])
+{
 	//checks if line is valid
-	
-	size_t x = pos[1];
-	size_t y = pos[0];
-	int cur = tab[y][x];
-	
+
+	int cur = tab[*y][*x];
 	for(size_t X = 0; X < 16; X++)
 	{
-		if (X != x && tab[y][X] == cur)
+		if (X != *x && tab[*y][X] == cur)
 			return 0;
 	}
 	return 1;
 }
 
-int check_column (size_t pos[2], int tab[][16])
-{ //done
+int check_column (size_t* x, size_t* y, int tab[][16])
+{
 	//checks if column is valid
-	
-	size_t x = pos[1];
-	size_t y = pos[0];
-	int cur = tab[y][x];
-	
+
+	int cur = tab[*y][*x];
 	for(size_t Y = 0; Y < 16; Y++)
 	{
-		if (Y != y && tab[Y][x] == cur)
+		if (Y != *y && tab[Y][*x] == cur)
 			return 0;
 	}
 	return 1;
 }
 
-int check_box (size_t pos[2], int tab[][16])
-{ //done
+int check_box (size_t* x, size_t* y, int tab[][16])
+{
 	//checks if box is valid
-	
-	size_t x = pos[1];
-	size_t y = pos[0];
-	int cur = tab[y][x];
-	
-	size_t xbias = (x / 4) * 4;
-	size_t ybias = (y / 4) * 4;
+
+	int cur = tab[*y][*x];
+	size_t xbias = (*x / 4) * 4;
+	size_t ybias = (*y / 4) * 4;
 	
 	for (size_t i = ybias; i < ybias + 4; i++)
 	{
 		for (size_t j = xbias; j < xbias + 4; j++)
 		{
-			if(!(j == x && i == y) && tab[i][j] == cur)
+			if(!(j == *x && i == *y) && tab[i][j] == cur)
 				return 0;
 		}
 	}
 	return 1;
 }
 
-void next (size_t pos[2], int ref[][16])
-{ //done
+void next (size_t* x, size_t* y, int ref[][16])
+{
 	//moves pos to the next mutable position
-	size_t x = pos[1];
-	size_t y = pos[0];
+
 	do {
-		x = (x + 1) % 16;
-		y += !(x);
-	} while (ref[y][x] >= 0);
-	pos[1] = x;
-	pos[0] = y;
+		*x = (*x + 1) % 16;
+		*y += !(*x);
+	} while (ref[*y][*x] >= 0);
 }
 
-void prev (size_t pos[2], int ref[][16])
-{ //done
+void prev (size_t* x, size_t* y, int ref[][16])
+{
 	//moves pos to the previous mutable position
-	size_t x = pos[1];
-	size_t y = pos[0];
 	do {
-		if (!x)
+		if (!*x)
 		{
-			y--;
-			x = 15;
+			*y = *y - 1;
+			*x = 15;
 		}
 		else 
-			x--;
-	} while (ref[y][x] >= 0);
-	pos[1] = x;
-	pos[0] = y;
+			*x = *x - 1;
+	} while (ref[*y][*x] >= 0);
 }
 
-int possible (size_t pos[2], int tab[][16])
-{ //done
+int possible (size_t* x, size_t* y, int tab[][16])
+{
 	//returns 0 if the current state of the board is impossible, else not 0
 	//printf("exec: possible\n");
-	return (check_line(pos, tab) && check_column(pos, tab)) && check_box(pos, tab);
+	return (check_line(x, y, tab) && check_column(x, y, tab)) && check_box(x, y, tab);
 }
 
-void mv_next (size_t pos[2], int tab[][16], int ref[][16])
-{ //done
+void mv_next (size_t* x, size_t* y, int tab[][16], int ref[][16])
+{
 	//moves the program forward in the board
-	
-	size_t x = pos[1];
-	size_t y = pos[0];
-	
-	if (possible(pos,tab))
+	if (possible(x, y, tab))
 	{
-		next(pos,ref);
-		tab[pos[0]][pos[1]]++;
+		next(x, y,ref);
+		tab[*y][*x]++;
 	}
-	else tab[y][x]++;
+	else tab[*y][*x]++;
 }
 
-void mv_prev (size_t pos[2], int tab[][16], int ref[][16])
+void mv_prev (size_t* x, size_t* y, int tab[][16], int ref[][16])
 { //done
 	//backtracks to next iterable state
-	
-	size_t x = pos[1];
-	size_t y = pos[0];
-	
 	do {
-		tab[y][x] = -1;
-		prev(pos,ref);
-		x = pos[1];
-		y = pos[0];
-	} while (tab[y][x] == 15);
-	tab[y][x]++;
+		tab[*y][*x] = -1;
+		prev(x, y,ref);
+	} while (tab[*y][*x] == 15);
+	tab[*y][*x]++;
 }
 
 
@@ -135,40 +109,27 @@ void solve (int tab[][16])
 	for(size_t x = 0; x < 16; x++)
 		for(size_t y = 0; y < 16; y++)
 			ref[x][y] = tab[x][y];
-	size_t p[] = {0,0}; //array representing the current position
-	//int i = 0;
+	size_t posx = 0;
+	size_t posy = 0;
 	while(1)
 	{
-		//i++;
-		//printf("\033[25A");
-		//printf("\033[25D");
-		//print_sudoku(tab);
-		
-		//for(int j = 0; j < 10000; j++)
-		//{}
-			//iteration
-		size_t x = p[1];
-		size_t y = p[0];
-		
-		if (y > 15)
+		if (posy > 15)
 		{
-			p[1] = 15;
-			p[0] = 15;
-			if (possible(p,tab))
+			posx = 15;
+			posy = 15;
+			if (possible(&posx, &posy, tab))
 				break;
 			else 
 			{
-				p[1] = 15;
-				p[0] = 15;
 				if (ref[15][15])
-					prev(p,ref);
-				mv_prev(p,tab,ref);
+					prev(&posx, &posy, ref);
+				mv_prev(&posx, &posy, tab,ref);
 			}
 		}
-		if (!possible(p,tab) && tab[y][x] == 15)
-			mv_prev(p,tab,ref);
+		if (!possible(&posx, &posy, tab) && tab[posy][posx] == 15)
+			mv_prev(&posx, &posy, tab,ref);
 		else 
-			mv_next(p,tab,ref);
+			mv_next(&posx, &posy, tab,ref);
 	}
 	
 }
