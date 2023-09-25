b44cc409a4df1023c180cc2d12426dac8594c085
elena <gaspard.souvay@epita.fr>
2023-09-25 20:34:28 +0100

    added comments

--- neural-network/structures/layer.h
+++ neural-network/structures/layer.h
@@ -11,9 +11,12 @@ typedef struct Layer
 }Layer;
 
 double get(Layer* layer, size_t x, size_t y);
+//returns layer->weights[y][x]
 
 void set(Layer* layer, size_t x, size_t y, double d);
+//sets layer->weights[y][x] to d
 
 double* compute_output(Layer* layer, double* input, size_t input_len);
+//computes the outputs of the layer for given inputs
 
 #endif
