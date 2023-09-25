a2c66beebb41644fa2640d95008fe42b197138e1
gaspard souvay <gaspard.souvay@epita.fr>
2023-09-25 13:29:09 +0200

    deleted files

--- neural-network/training/image/image.h
+++
@@ -1,7 +0,0 @@
-#ifndef IMAGE_H
-#define IMAGE_H
-
-void read_image(size_t n, int rows, int columns, char t[][columns], char* file)
-void get_size(char* file, int* rows, int* columns, int* images);
-
-#endif
